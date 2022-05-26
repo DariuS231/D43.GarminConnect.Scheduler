e5ba92be80938372f8eb80dfae8e749440670cbc
Dario Alvarez <dario17231@gmail.com>
2022-05-26 11:47:35 +0100

    Implemented eslint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    project: './tsconfig.json',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    semi: [ 'error', 'always' ]
+  }
+}
